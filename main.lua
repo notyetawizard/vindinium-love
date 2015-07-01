0fb7550bec17df763d9e999cd164bb692614f68c
notyetawizard <notyetawizard@gmail.com>
2015-07-01 15:24:55 -0500

    Adding the map drawing system, but the canvas currently disapears after one turn.

--- main.lua
+++ main.lua
@@ -3,7 +3,8 @@ local json = require "lib/dkjson"
 -- Temp bot loading. Will need to clean this up.
 local nbot = require "bots/notyetawizard"
 
--- Initiate some variables
+-- Initiate some global variables that bots will
+-- need access to.
 game = {}
 map = {taverns = {}, mines = {}, heros = true, hero = true}
 
@@ -69,12 +70,15 @@ local function correct_pos(pos)
     pos.x, pos.y = pos.y + 1, pos.x + 1
 end
 
+-- Turn the position of a tile in the string
+-- into x and y values
 local function correct_xy(a,b)
     x = (a - 1) / 2 % map.size + 1
     y = ((b / 2) - x) / map.size + 1
     return x, y
 end
 
+-- Replace the heroes last positions with ground on the map.
 local function remove_heroes()
     for i = 1, 4 do
         map[map.heroes[i].pos.y][map.heroes[i].pos.x] = 0
@@ -111,8 +115,8 @@ local function update_map(board, tile, store)
     end
 end
 
-function parse_first_response(response)
-    -- Parse response json
+-- Map parsers
+local function parse_first_response(response)
     local parsed = json.decode(response)
     game.finished = parsed.game.finished
     game.id = parsed.game.id
@@ -139,8 +143,7 @@ function parse_first_response(response)
     update_map(board, valid_tiles.tavern, map.taverns)
 end
 
-function parse_response(response)
-    -- Parse response json
+local function parse_response(response)
     local parsed = json.decode(response)
     game.finished = parsed.game.finished
     game.turn = parsed.game.turn
@@ -161,8 +164,70 @@ function parse_response(response)
     update_map(board, valid_tiles.mine, map.mines)
 end
 
-function random_move()
+-- Function that returns a random valid move.
+-- Good for testing and example bots.
+function game.random_move()
     return valid_moves[math.random(1,5)]
+end
+
+--
+-- Set filtering to nearest neighbor, for sprite art
+love.graphics.setDefaultFilter("nearest", "nearest")
+
+-- Load all needed images
+local gfx = {
+    tile_size = 24,
+    tile_offset = 8,
+    [0] = love.graphics.newImage("gfx/0_ground.png"),
+    [1] = {
+        north = love.graphics.newImage("gfx/1_heroN.png"),
+        east = love.graphics.newImage("gfx/1_heroE.png"),
+        south = love.graphics.newImage("gfx/1_heroS.png"),
+        west = love.graphics.newImage("gfx/1_heroW.png")
+    },
+    [2] = {
+        north = love.graphics.newImage("gfx/2_heroN.png"),
+        east = love.graphics.newImage("gfx/2_heroE.png"),
+        south = love.graphics.newImage("gfx/2_heroS.png"),
+        west = love.graphics.newImage("gfx/2_heroW.png")
+    },
+    [3] = {
+        north = love.graphics.newImage("gfx/3_heroN.png"),
+        east = love.graphics.newImage("gfx/3_heroE.png"),
+        south = love.graphics.newImage("gfx/3_heroS.png"),
+        west = love.graphics.newImage("gfx/3_heroW.png")
+    },
+    [4] = {
+        north = love.graphics.newImage("gfx/4_heroN.png"),
+        east = love.graphics.newImage("gfx/4_heroE.png"),
+        south = love.graphics.newImage("gfx/4_heroS.png"),
+        west = love.graphics.newImage("gfx/4_heroW.png")
+    },
+    [5] = love.graphics.newImage("gfx/5_mine.png"),
+    [6] = love.graphics.newImage("gfx/6_mine.png"),
+    [7] = love.graphics.newImage("gfx/7_mine.png"),
+    [8] = love.graphics.newImage("gfx/8_mine.png"),
+    [9] = love.graphics.newImage("gfx/9_mine.png"),
+    [10] = love.graphics.newImage("gfx/10_tavern.png")
+}
+
+-- Draw the map to a canvas, as it never changes.
+local function draw_map()
+    local map_canvas = love.graphics.newCanvas(map.size*gfx.tile_size,
+        map.size*gfx.tile_size),
+    love.graphics.setCanvas(map_canvas)
+    for y = 1, map.size do
+        if map[y] then
+            for x = 1, map.size do
+                if map[y][x] ~= 11 then
+                    love.graphics.draw(gfx[0], x*gfx.tile_size,
+                        y*(gfx.tile_size-gfx.tile_offset))
+                end
+            end
+        end
+    end
+    love.graphics.setCanvas()
+    gfx.map = love.graphics.newImage(map_canvas:getImageData())
 end
 
 ---
@@ -195,5 +260,16 @@ function love.update()
 end
 
 function love.draw()
+    -- init the map if it doesn't exist
+    if not gfx.map then
+        draw_map()
+    end
+    -- draw the map
+    love.graphics.draw(gfx.map, 1, 1)
 
+    -- draw a test sprite every other turn, because I
+    -- can't figure out why the map disappears and this doesn't.
+    if game.turn % 8 == 0 then
+        love.graphics.draw(gfx[10], 24, 25)
+    end
 end
